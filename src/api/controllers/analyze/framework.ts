154e587c92cdf1cf9c955689163c5585f7ecf9a5
Aleksei Korobkin <aleksei.korobkin@akvelon.com>
2023-02-16 14:06:04 +0100

    iOS detection initially implemented

--- src/api/controllers/analyze/framework.ts
+++ src/api/controllers/analyze/framework.ts
@@ -1,7 +1,58 @@
 import { Request, Response } from 'express';
+import { https } from 'follow-redirects';
+import fs from 'fs';
+import path from 'path';
+import extract from 'extract-zip';
+import xcode from 'xcode';
 
-export function analyzeFramework(req: Request, res: Response) {
-    res.send({
-        message: 'Analyzer will work here..',
+export async function analyzeFramework(req: Request, res: Response) {
+
+    fs.rmSync(process.env.TEMPDIRPATH, { recursive: true, force: true } );    
+    fs.mkdirSync(process.env.TEMPDIRPATH);
+
+    const file = fs.createWriteStream(process.env.TEMPDIRPATH + '\\'+ process.env.TEMPFILENAME);
+    const sourcerype = req.body['SourceType'];
+    const link = req.body['Link'];
+
+    const request = https.get(link, function(response) {
+        response.pipe(file);
+
+        file.on('finish', async () => {
+            
+            file.close();
+            console.log('Download Completed');
+
+            await extract(path.resolve(file.path.toString()), {dir: path.resolve(process.env.TEMPDIRPATH).toString() });
+        
+            const fileCb = function(file) {
+                console.log('Scanned file:'+ file.path);
+                
+                const compVersion = xcode.project(file.path)
+                    .parseSync().getFirstProject()
+                    .firstProject.compatibilityVersion
+                    .replace(/^"(.*)"$/, '$1');
+            
+                console.log('Found compatibilityVersion: ' + compVersion);
+
+                res.setHeader('Content-Type', 'application/json');
+                res.send(JSON.stringify({ compatibilityVersion: compVersion }, null, 3));
+                
+            };
+            const dirCb = function(directory) {
+                console.log('Scanned folder:'+ directory.path);
+            };
+
+            const dree = require('dree');
+            dree.scan(path.resolve('tmp').toString(), { extensions: [ 'pbxproj' ] }, fileCb, dirCb);
+
+        });
     });
+
+
+  
+
+
+
+
+
 }
