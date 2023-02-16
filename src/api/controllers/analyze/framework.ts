9c822773c6c14c3f33aab6421a41429883247d95
Aleksei Korobkin <aleksei.korobkin@akvelon.com>
2023-02-16 21:11:33 +0100

    Project file search simplified, code formatted

--- src/api/controllers/analyze/framework.ts
+++ src/api/controllers/analyze/framework.ts
@@ -4,7 +4,6 @@ import fs from 'fs';
 import path from 'path';
 import extract from 'extract-zip';
 import xcode from 'xcode';
-
 import stream from 'node:stream/promises';
 import { Readable } from 'stream';
 
@@ -14,74 +13,67 @@ async function httpsPromisedGet(link): Promise<Readable> {
     });
 }
 
-
-function findFilesByExt(base,ext,files,result) 
-{
-    files = files || fs.readdirSync(base); 
-    result = result || []; 
-
-    files.forEach( 
-        function (file) {
-            const newbase = path.join(base,file);
-            if ( fs.statSync(newbase).isDirectory() )
-            {
-                result = findFilesByExt(newbase,ext,fs.readdirSync(newbase),result);
-            }
-            else
-            {
-                if ( file.substr(-1*(ext.length+1)) == '.' + ext )
-                {
-                    result.push(newbase);
-                } 
+function findFileByExt(base, ext): string {
+    
+    var files = fs.readdirSync(base);
+    var result = "";
+    
+    for (let i = 0; i < files.length; i++) {
+        
+        const newbase = path.join(base, files[i]);
+        
+        if (fs.statSync(newbase).isDirectory()) {
+            result = findFileByExt(newbase, ext);
+            if (result.length > 0) {
+                break;
             }
         }
-    );
+        else if (fs.statSync(newbase).isFile() && (files[i].substring((ext.length + 1)) === ext)) {
+            result = newbase;
+            break;
+        }
+    }
+    
     return result;
+
 }
 
 export async function analyzeFramework(req: Request, res: Response) {
-
     const tempDir = path.resolve(process.env.TEMPDIRPATH);
     const tempFile = path.join(tempDir, process.env.TEMPFILENAME);
     
-    fs.rmSync(tempDir, { recursive: true, force: true } );    
+    fs.rmSync(tempDir, { recursive: true, force: true });
     fs.mkdirSync(tempDir);
-
     const file = fs.createWriteStream(tempFile);
+    
     const sourcerype = req.body['SourceType'];
     const link = req.body['Link'];
-
+    
     const response = await httpsPromisedGet(link);
-
     await stream.pipeline(response, file);
-
     console.log('Download Completed');
-
-    await extract(tempFile, {dir: tempDir });
+    await extract(tempFile, { dir: tempDir });
     console.log('Sources extracted');
 
     const EXTENSION = 'pbxproj';
+    const projFile = findFileByExt(tempDir, EXTENSION);
+    if (projFile.length > 0) {
 
-    const projFiles = findFilesByExt(tempDir, EXTENSION, null , null);
-
-    if (projFiles.length > 0){
-    
-        const compVersion = xcode.project(projFiles[0])
+        const compVersion = xcode.project(projFile)
             .parseSync().getFirstProject()
             .firstProject.compatibilityVersion
             .replace(/^"(.*)"$/, '$1');
 
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({ compatibilityVersion: compVersion }, null, 3));
-    
+
         console.log('Found');
     }
-    else
-    {
-        if (!res.writableEnded)
-        {
+    else {
+        if (!res.writableEnded) {
             res.end('Noone supported framework detected');
+            console.log('Not found');
         }
     }
-
+    
 }
