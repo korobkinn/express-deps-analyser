42b18b27b6459c5352d3b701a18abb7889f8fdf6
Aleksei Korobkin <aleksei.korobkin@akvelon.com>
2023-02-16 18:58:02 +0100

    Parser lined-up

--- src/api/controllers/analyze/framework.ts
+++ src/api/controllers/analyze/framework.ts
@@ -5,59 +5,83 @@ import path from 'path';
 import extract from 'extract-zip';
 import xcode from 'xcode';
 
+import stream from 'node:stream/promises';
+import { Readable } from 'stream';
+
+async function httpsPromisedGet(link): Promise<Readable> {
+    return new Promise((resolve, reject) => {
+        https.get(link, (response) => resolve(response)).on('error', reject);
+    });
+}
+
+
+function findFilesByExt(base,ext,files,result) 
+{
+    files = files || fs.readdirSync(base); 
+    result = result || []; 
+
+    files.forEach( 
+        function (file) {
+            const newbase = path.join(base,file);
+            if ( fs.statSync(newbase).isDirectory() )
+            {
+                result = findFilesByExt(newbase,ext,fs.readdirSync(newbase),result);
+            }
+            else
+            {
+                if ( file.substr(-1*(ext.length+1)) == '.' + ext )
+                {
+                    result.push(newbase);
+                } 
+            }
+        }
+    );
+    return result;
+}
+
 export async function analyzeFramework(req: Request, res: Response) {
 
-    fs.rmSync(process.env.TEMPDIRPATH, { recursive: true, force: true } );    
-    fs.mkdirSync(process.env.TEMPDIRPATH);
+    const tempDir = path.resolve(process.env.TEMPDIRPATH);
+    const tempFile = path.join(tempDir, process.env.TEMPFILENAME);
+    
+    fs.rmSync(tempDir, { recursive: true, force: true } );    
+    fs.mkdirSync(tempDir);
 
-    const file = fs.createWriteStream(process.env.TEMPDIRPATH + '\\'+ process.env.TEMPFILENAME);
+    const file = fs.createWriteStream(tempFile);
     const sourcerype = req.body['SourceType'];
     const link = req.body['Link'];
 
-    const request = https.get(link, function(response) {
-        response.pipe(file);
+    const response = await httpsPromisedGet(link);
 
-        file.on('finish', async () => {
-            
-            file.close();
-            console.log('Download Completed');
+    await stream.pipeline(response, file);
 
-            await extract(path.resolve(file.path.toString()), {dir: path.resolve(process.env.TEMPDIRPATH).toString() });
-        
-            const fileCb = function(file) {
-                console.log('Scanned file:'+ file.path);
-                
-                const compVersion = xcode.project(file.path)
-                    .parseSync().getFirstProject()
-                    .firstProject.compatibilityVersion
-                    .replace(/^"(.*)"$/, '$1');
-            
-                console.log('Found compatibilityVersion: ' + compVersion);
+    console.log('Download Completed');
 
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify({ compatibilityVersion: compVersion }, null, 3));
-                
-            };
-            const dirCb = function(directory) {
-                console.log('Scanned folder:'+ directory.path);
-            };
+    await extract(tempFile, {dir: tempDir });
+    console.log('Sources extracted');
 
-            const dree = require('dree');
-            await dree.scan(path.resolve('tmp').toString(), { extensions: [ 'pbxproj' ] }, fileCb, dirCb);
-            
-            if (!res.writableEnded)
-            {
-                res.end("Noone supported framework detected");
-            }
+    const EXTENSION = 'pbxproj';
 
-        });
-    });
+    const projFiles = findFilesByExt(tempDir, EXTENSION, null , null);
 
+    if (projFiles.length > 0){
+    
+        const compVersion = xcode.project(projFiles[0])
+            .parseSync().getFirstProject()
+            .firstProject.compatibilityVersion
+            .replace(/^"(.*)"$/, '$1');
 
-  
-
-
-
-
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({ compatibilityVersion: compVersion }, null, 3));
+    
+        console.log('Found');
+    }
+    else
+    {
+        if (!res.writableEnded)
+        {
+            res.end('Noone supported framework detected');
+        }
+    }
 
 }
