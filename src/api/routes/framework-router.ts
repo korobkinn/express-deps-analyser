10bd98fe84b5fd20ca39813461455ebdd953b9a3
Aleksei Korobkin <aleksei.korobkin@akvelon.com>
2023-02-17 09:25:13 +0100

    Unneeded router excluded

--- src/api/routes/framework-router.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { analyzeFramework } from '../controllers/analyze-framework';
-
-export const analyzeFrameworkRouter = Router();
-
-analyzeFrameworkRouter.post('/', analyzeFramework);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { analyzeFrameworkRouter } from './api/routes/framework-router';
+import { analyzeFramework } from './api/controllers/analyze-framework';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
-app.use('/api/v1/analyze/framework', analyzeFrameworkRouter);
+
+app.post('/api/v1/analyze/framework', analyzeFramework);
 app.listen(port, () => console.log(`Running on port ${port}`));
