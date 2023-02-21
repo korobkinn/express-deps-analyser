c2c48f2bdead5149f990a5348b6ec8853d9690af
Aleksei Korobkin <aleksei.korobkin@akvelon.com>
2023-02-21 15:23:02 +0100

    xcode parser test extended

--- tests/parsers/xcode.test.ts
+++ tests/parsers/xcode.test.ts
@@ -8,4 +8,15 @@ describe('XCODE parser test', () => {
         console.log(result);
         expect(result).to.deep.equal({ compatibilityVersion: 'Xcode 14.0' });
     });
+    it('Should detect XCODE project without compatibilityVersion information', async function () {
+        const result = new xcode().parse(path.resolve('tests/assets/xcode-example-project-nocompat'));
+        console.log(result);
+        expect(result).to.deep.equal({ warning: 'Found XCODE project file, but without compatibility information' });
+    });
+    it('Should return error for non-detectable projects', async function () {
+        const result = new xcode().parse(path.resolve('tests/assets/dotnet-example-project'));
+        console.log(result);
+        expect(result).to.deep.equal({ error: 'Noone supported framework detected' });
+    });
+
 });
